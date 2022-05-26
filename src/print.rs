a047a708b2b509f3da861bf5f546724ae4e62ecb
Thuble <thuble@protonmail.comm>
2022-05-26 22:31:57 +1000

    Adding more formatting

--- src/print.rs
+++ src/print.rs
@@ -1,7 +1,16 @@
 pub fn run() {
+    // Print to console
     println!("Hello, world!");
 
+    // Basic formatting
     println!("{} days", 31);
 
+    // Positional arguments
     println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+
+    // Named arguments
+    println!("{subject} {verb} {object}",
+            object="the lazy dog",
+            subject="the quick brown fox",
+            verb="jumps over");
 }
