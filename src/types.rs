00cbe601bb11acd5c5bb573f1ed7ed0e5775edcf
Thuble <thuble@protonmail.comm>
2022-05-26 22:58:50 +1000

    Adding booleans

--- src/types.rs
+++ src/types.rs
@@ -1,11 +1,24 @@
 pub fn run() {
-    let x = 1;
+    let _x = 1;
 
-    let y = 2.5;
+    let _y = 2.5;
 
     // Explicit type
-    let y: i64 = 324267890235;
+    let _z: i64 = 324267890235;
 
     println!("Max i32: {}", std::i32::MAX);
     println!("Max i64: {}", std::i64::MAX);
+
+    // Boolean
+    let is_active = true;
+
+    // Get boolean from expression
+    let is_greater: bool = 10 > 5;
+
+    let a1 = 'a';
+
+    let face = '\u{1F600}';
+
+    println!("{:?}", (a1, face));
+    println!("{:?}", (is_active, _x, _y, _z, is_greater));
 }
