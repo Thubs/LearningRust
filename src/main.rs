afe992708e378161e325ac71d520e1879df7d93b
Thubs <hughcookpalmeh@gmail.com>
2022-05-31 17:28:15 +1000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,5 @@
 #![allow(non_snake_case)]
 
-// mod print;
-// mod vars;
-// mod types;
-// mod strings;
-// mod tuples;
-// mod arrays;
-// mod vectors;
-mod Casting;
-
 fn main() {
-    Casting::run();
+    // Placeholding
 }
